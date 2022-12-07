52020de13270573c94c33aafe142433dbb6cfd91
leonliu1008 <leonliu1008@gmail.com>
2022-12-07 17:30:05 +0800

    upload

--- client/src/pages/Word.js
+++ client/src/pages/Word.js
@@ -3,6 +3,8 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function Word(){
+    const [title, setTitle] = useState('');
+
     //宣告一個新的state變數 ,我叫他"count"
     const [count,setCount] = useState(0)
 
@@ -11,10 +13,22 @@ export default function Word(){
       // 使用瀏覽器 API 更新文件標題
       document.title = `You clicked ${count} times`;
     });
+    useEffect(()=> {
+      console.log(title);
+     }, [title]);
     
     return (
       <div className="">
           <Header/>
+          <div className="w-1/2 h-auto rounded-lg drop-shadow-md overflow-hidden bg-white p-10">
+            <input
+                type="text"
+                placeholder={'標題'}
+                value={title} maxLength={100}
+                onChange={(e) => setTitle(e.target.value)}
+                className="w-full h-10 rounded-md bg-gray-50 px-4 py-2 mb-10 focus:outline-0 focus:bg-yellow-700/5">
+             </input>
+          </div>
           <p className="text-center text-3xl mt-20">This is TEST</p>
           <p className="text-center text-3xl mt-20">此為測試頁面</p>
           <p className="text-center text-3xl mt-20">You clicked {count} times</p>
